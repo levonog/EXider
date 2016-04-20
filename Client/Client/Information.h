afcdbc1a7480b3480374a1a51d106fb3cfd2ed7d
StFreve <Golfik2007@gmail.com>
2016-04-20 21:35:08 +0400

    Update 20.04.2016

--- Client/Client/Information.h
+++ Client/Client/Information.h
@@ -12,18 +12,18 @@ namespace EXider {
 		};
 		Information( const Client* client );
 		
-		void taskList();											// List of tasks with results
-		void taskInformation(  std::vector<size_t> tIDs );			// Full information about tasks
+		void taskList();                                            // List of tasks with results
+		void taskInformation(  std::vector<size_t> tIDs );          // Full information about tasks
 
-		void pcList( size_t Status = Available);					// List of RemotePCs
+		void pcList( size_t Status = Available);                    // List of RemotePCs
 
-		void help(size_t CommandTypes = (PC | Task | Usage) );		// Exider's commands instructions
+		void help(size_t CommandTypes = (PC | Task | Usage) );      // Exider's commands instructions
 
-		void error( const std::string& message ) const;				// Error message
-		void warning( const std::string& message ) const;			// Warning message
-		void wrongArgument( const std::string& arg ) const;			// Message of incorrect argument
-		void wrongParameter( const std::string& param ) const;		// Message of incorrect parameter
-		void wrongCommand( const std::string& command ) const;		// Message of incorrect command
-		void print( const std::string& message ) const;				// Print message
+		void error( const std::string& message ) const;	            // Error message
+		void warning( const std::string& message ) const;           // Warning message
+		void wrongArgument( const std::string& arg ) const;	        // Message of incorrect argument
+		void wrongParameter( const std::string& param ) const;      // Message of incorrect parameter
+		void wrongCommand( const std::string& command ) const;      // Message of incorrect command
+		void print( const std::string& message ) const;             // Print message
 	};
 }
--- Client/Client/RemotePC.h
+++ Client/Client/RemotePC.h
@@ -2,39 +2,39 @@
 #include "EXider.h"
 namespace EXider {
 
-	class RemotePC : public boost::enable_shared_from_this<RemotePC>{
-		boost::asio::ip::tcp::socket m_socket;
-		boost::asio::ip::tcp::endpoint m_endpoint;
-		boost::asio::streambuf m_buffer;
-		boost::function<void(boost::shared_ptr<RemotePC>, std::string )> m_callback;
-		
-		rpcStatus m_status;
+    class RemotePC : public boost::enable_shared_from_this<RemotePC> {
+        boost::asio::ip::tcp::socket m_socket;
+        boost::asio::ip::tcp::endpoint m_endpoint;
+        boost::asio::streambuf m_buffer;
+        boost::function<void( boost::shared_ptr<RemotePC>, std::string )> m_callback;
 
-		int m_id;
-		// Async Wrtie, Read, Connecting Handlers
-		void sendHandler( const boost::system::error_code& error );
-		void readHandler( const boost::system::error_code& error, size_t bytes );
-		void connectedHandler( const boost::system::error_code& error );
+        rpcStatus m_status;
 
-	public:
-		RemotePC( boost::asio::io_service& io, const std::string& IP );
-		RemotePC( boost::asio::io_service& io, const boost::asio::ip::address& IP );
-		bool connect();											// Sync-Connect to the socket
-		bool reconnect();										// Reconnecting to the socket
-		void disconnect();										// Disconnecting the socke
-		inline const rpcStatus status() const;					// PC's current status
-		
-		void sendRequest( const std::string& request );			// Async-Writing message in socket
-		void readRequest();										// Async-Reading message from socket
+        int m_id;
+        // Async Wrtie, Read, Connecting Handlers
+        void sendHandler( const boost::system::error_code& error );
+        void readHandler( const boost::system::error_code& error, size_t bytes );
+        void connectedHandler( const boost::system::error_code& error );
 
-		// Setup PC's ID in current Task, and call-back function to communicate with the Task
-		void setID( size_t ID );  
-		void setCallBackFunction( const boost::function<void( boost::shared_ptr<RemotePC> fromPC, std::string result )>& cb );
-	
-		boost::shared_ptr<RemotePC> getSelfPtr();				// Self Pointer
-		const int getID() const;								// PC's ID in current Task
-		inline const boost::asio::ip::address getIP() const;	// IP's IP address
-		bool operator==( const RemotePC& rhrp ) const;
-		bool operator<( const RemotePC& rhrp ) const;
-	};
+    public:
+        RemotePC( boost::asio::io_service& io, const std::string& IP );
+        RemotePC( boost::asio::io_service& io, const boost::asio::ip::address& IP );
+        bool connect();	                                        // Sync-Connect to the socket
+        bool reconnect();                                       // Reconnecting to the socket
+        void disconnect();                                      // Disconnecting the socke
+        inline const rpcStatus status() const;					// PC's current status
+
+        void sendRequest( const std::string& request );	        // Async-Writing message in socket
+        void readRequest();	                                    // Async-Reading message from socket
+
+        // Setup PC's ID in current Task, and call-back function to communicate with the Task
+        void setID( size_t ID );
+        void setCallBackFunction( const boost::function<void( boost::shared_ptr<RemotePC> fromPC, std::string result )>& cb );
+
+        boost::shared_ptr<RemotePC> getSelfPtr();               // Self Pointer
+        const int getID() const;                                // PC's ID in current Task
+        inline const boost::asio::ip::address getIP() const;    // IP's IP address
+        bool operator==( const RemotePC& rhrp ) const;
+        bool operator<( const RemotePC& rhrp ) const;
+    };
 };
