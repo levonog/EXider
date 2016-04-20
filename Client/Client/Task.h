94b2fc246a6a397243117e35f25e71b81165e780
StFreve <Golfik2007@gmail.com>
2016-04-20 21:36:37 +0400

    Update 20.04.2016

--- Client/Client/Task.h
+++ Client/Client/Task.h
@@ -3,18 +3,18 @@
 namespace EXider {
 	class Task {
 		boost::asio::io_service& m_io;
-		mutable boost::recursive_mutex m_mutexForResult;					// Mutex to read or write resulting information
+		mutable boost::recursive_mutex m_mutexForResult;                                // Mutex to read or write resulting information
 		
 		const std::string m_taskName;
 		const size_t m_tID;
-		PCList m_workingPCs;										// PC's working on the task
-		std::vector<std::string> m_result;							// Results received from RemotePC's
+		PCList m_workingPCs;                                                            // PC's working on the task
+		std::vector<std::string> m_result;                                              // Results received from RemotePC's
 
-		const std::string m_downloadURL, m_executeCommand;			// Download URL (ftp) and Executeing Command for RemotePC
+		const std::string m_downloadURL, m_executeCommand;                              // Download URL (ftp) and Executeing Command for RemotePC
 		bool m_downloadsFiles;
-		bool m_autoFree;											// Automaticly delete PC from the task, when result is received
+		bool m_autoFree;                                                                // Automaticly delete PC from the task, when result is received
 		
-		void handler( boost::shared_ptr<RemotePC> fromPC, std::string result );			// call-back function for RemotePC
+		void handler( boost::shared_ptr<RemotePC> fromPC, std::string result );	        // call-back function for RemotePC
 
 		boost::thread m_thread;
 	public:
